f3058adce989c41626312939367e8b629caae378
Seunggyun Kim <moonrise917@gmail.com>
2018-10-09 13:50:56 +0900

    Update license, change build.gradle to target v1.0.2

--- src/main/java/kr/projectn/vdl/core/submodule/social/instagram.java
+++ src/main/java/kr/projectn/vdl/core/submodule/social/instagram.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016-2018 qscx9512 <moonrise917@gmail.com>
+ * Copyright 2016-2018 polaris9017 <moonrise917@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /*
  * Instagram media JSON structure (August, 2018)
  *
